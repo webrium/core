d48ace34ac68377add25406c04cc70177e26a9b4
سید بنیامین خلیفه <nvioir@gmail.com>
2020-05-27 23:49:31 +0430

    add http response code func

--- src/Debug.php
+++ src/Debug.php
@@ -3,12 +3,23 @@ namespace webrium\core;
 
 class Debug
 {
-  public function displayErrors($status)
+  public static function displayErrors($status)
   {
     if ($status) {
       ini_set('display_errors', $status);
       ini_set('display_startup_errors', $status);
       error_reporting(E_ALL);
     }
+  }
+
+  public static function code($code)
+  {
+    http_response_code($code);
+  }
+
+  public static function error404()
+  {
+    self::code(404);
+    echo "404";
   }
 }
