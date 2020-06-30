cc25670cace2e90b6520002ed11caca28b0b1ceb
سید بنیامین خلیفه <nvioir@gmail.com>
2020-06-30 11:22:38 +0430

    add mail class

---
+++ src/Mail.php
@@ -0,0 +1,18 @@
+<?php
+namespace webrium\core;
+
+class Mail
+{
+  private static $config=[];
+
+  public static function addConfig($name='main',$mail)
+  {
+    self::$config[$name]=$mail;
+  }
+
+  public static function get($name = 'main'){
+    return self::$config[$name];
+  }
+
+
+}
