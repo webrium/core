7a5e58c6cfeac92b2f11098d6e7a8cf6f93f6d1e
سید بنیامین خلیفه <nvioir@gmail.com>
2020-07-16 22:16:40 +0430

    update view blade

--- src/View.php
+++ src/View.php
@@ -37,21 +37,21 @@ class View
 
       $code = $str. File::getContent($file_path);
 
-      self::str_replace_type_value('/\@foreach\((.+?\W+)\)/','<?php foreach',": ?>",$code);
-      self::str_replace_type_value('/\@foreach[[:blank:]]\((.+?\W+)\)/','<?php foreach',": ?>",$code);
+      self::str_replace_type_value('/\@foreach\((.+?\W+|.+)\)/','<?php foreach',": ?>",$code);
+      self::str_replace_type_value('/\@foreach[[:blank:]]\((.+?\W+|.+)\)/','<?php foreach',": ?>",$code);
 
-      self::str_replace_type_value('/\@for\((.+?\W+)\)/','<?php for',": ?>",$code);
-      self::str_replace_type_value('/\@for[[:blank:]]\((.+?\W+)\)/','<?php for',": ?>",$code);
+      self::str_replace_type_value('/\@for\((.+?\W+|.+)\)/','<?php for',": ?>",$code);
+      self::str_replace_type_value('/\@for[[:blank:]]\((.+?\W+|.+)\)/','<?php for',": ?>",$code);
 
-      self::str_replace_type_value('/\@if\((.+?\W+)\)/','<?php if',": ?>",$code);
-      self::str_replace_type_value('/\@if[[:blank:]]\((.+?\W+)\)/','<?php if',": ?>",$code);
+      self::str_replace_type_value('/\@if\((.+?\W+|.+)\)/','<?php if',": ?>",$code);
+      self::str_replace_type_value('/\@if[[:blank:]]\((.+?\W+|.+)\)/','<?php if',": ?>",$code);
 
-      self::str_replace_type_value('/\@elseif\((.+?\W+)\)/','<?php elseif',": ?>",$code);
-      self::str_replace_type_value('/\@else[[:blank:]]if\((.+?\W+)\)/','<?php elseif',": ?>",$code);
+      self::str_replace_type_value('/\@elseif\((.+?\W+|.+)\)/','<?php elseif',": ?>",$code);
+      self::str_replace_type_value('/\@else[[:blank:]]if\((.+?\W+|.+)\)/','<?php elseif',": ?>",$code);
 
-      self::str_replace_type_value('/\@echo\((.+?\W+)\)/','<?php echo',"; ?>",$code);
-      self::str_replace_type_value('/\@view\((.+?\W+)\)/','<?= view',"; ?>",$code);
-      self::str_replace_type_value('/\@url\((.+?\W+)\)/','<?= url',"; ?>",$code);
+      self::str_replace_type_value('/\@echo\((.+?\W+|.+)\)/','<?php echo',"; ?>",$code);
+      self::str_replace_type_value('/\@view\((.+?\W+|.+)\)/','<?= view',"; ?>",$code);
+      self::str_replace_type_value('/\@url\((.+?\W+|.+)\)/','<?= url',"; ?>",$code);
 
       self::str_replace_type_value('/\{{(.+?)\}}/','<?php echo htmlspecialchars',"; ?>",$code);
       self::str_replace_type_value('/\{!!(.+?)\!!}/','<?php echo ',"; ?>",$code,false);
@@ -80,7 +80,10 @@ class View
 
     foreach ($_str1 as $key => $value) {
 
+      $value = self::checkSyntax($value);
+
       $_r = $_str2[$key];
+      $_r = self::checkSyntax($_r);
 
       if ($parentheses) {
         $code = str_replace($value,"$to($_r)$end",$code);
@@ -88,19 +91,52 @@ class View
       else {
         $code = str_replace($value,"$to$_r$end",$code);
       }
+
     }
+  }
+
+  private static function checkSyntax($value)
+  {
+    $start = explode('(',$value);
+    $end = explode(')',$value);
+
+    $count_start = count($start);
+    $count_end = count($end);
+
+    if ($count_start != $count_end) {
+
+      $int = $count_end - $count_start;
+
+      for($i=0;$i<$int;$i++){
+        unset($end[$count_end-1]);
+      }
+
+      $new = implode(')',$end);
+
+      $count_start = count($start);
+      $count_end = count($end);
+
+      if (strrpos($new,')')>$count_end) {
+        $new = substr($new,0,strrpos($new,')')+1);
+      }
+
+      $value = $new;
+
+    }
+
+    return $value;
   }
 
 
   /**
-   * Get the MD5 hash of the file at the given path.
-   *
-   * @param  string  $path
-   * @return string
-   */
+  * Get the MD5 hash of the file at the given path.
+  *
+  * @param  string  $path
+  * @return string
+  */
   private static function hash($path)
   {
-      return md5_file($path);
+    return md5_file($path);
   }
 
   public static function clearCaches()
