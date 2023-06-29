be9396c33a056d6bbfbfee207357e1af4df8fb09
parsgit <nvioir@gmail.com>
2023-06-29 18:26:11 +0000

    Add comment to Hash class

--- src/Hash.php
+++ src/Hash.php
@@ -3,14 +3,28 @@ namespace Webrium;
 
 class Hash{
 
-  public static function make($text='')
-  {
-    return password_hash($text, PASSWORD_DEFAULT);
-  }
+/**
+ * Hash a text using the given algorithm.
+ *
+ * @param string $text The text to be hashed.
+ * @param int $algorithm [optional] The hashing algorithm to be used. Defaults to PASSWORD_DEFAULT.
+ * @return string The hashed text.
+ */
+public static function make($text='', $algorithm=PASSWORD_DEFAULT)
+{
+  return password_hash($text, $algorithm);
+}
 
-  public static function check($password,$hash)
-  {
-    return password_verify($password,$hash);
-  }
+/**
+ * Check if a password matches a certain hash.
+ *
+ * @param string $password The password to be checked.
+ * @param string $hash The hash to be compared against.
+ * @return bool Returns true if the password matches the hash, false otherwise.
+ */
+public static function check($password,$hash)
+{
+  return password_verify($password,$hash);
+}
 
 }
