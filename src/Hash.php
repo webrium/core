87ae520e10ed3130946f1ccb3cb248d28d5f9ab2
parsgit <nvioir@gmail.com>
2023-06-29 18:26:47 +0000

    Update Hash class

--- src/Hash.php
+++ src/Hash.php
@@ -1,30 +1,31 @@
 <?php
+
 namespace Webrium;
 
-class Hash{
-
-/**
- * Hash a text using the given algorithm.
- *
- * @param string $text The text to be hashed.
- * @param int $algorithm [optional] The hashing algorithm to be used. Defaults to PASSWORD_DEFAULT.
- * @return string The hashed text.
- */
-public static function make($text='', $algorithm=PASSWORD_DEFAULT)
+class Hash
 {
-  return password_hash($text, $algorithm);
-}
 
-/**
- * Check if a password matches a certain hash.
- *
- * @param string $password The password to be checked.
- * @param string $hash The hash to be compared against.
- * @return bool Returns true if the password matches the hash, false otherwise.
- */
-public static function check($password,$hash)
-{
-  return password_verify($password,$hash);
-}
+  /**
+   * Hash a text using the given algorithm.
+   *
+   * @param string $text The text to be hashed.
+   * @param int $algorithm [optional] The hashing algorithm to be used. Defaults to PASSWORD_DEFAULT.
+   * @return string The hashed text.
+   */
+  public static function make($text = '', $algorithm = PASSWORD_DEFAULT)
+  {
+    return password_hash($text, $algorithm);
+  }
 
+  /**
+   * Check if a password matches a certain hash.
+   *
+   * @param string $password The password to be checked.
+   * @param string $hash The hash to be compared against.
+   * @return bool Returns true if the password matches the hash, false otherwise.
+   */
+  public static function check($password, $hash)
+  {
+    return password_verify($password, $hash);
+  }
 }
