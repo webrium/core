354e9316a4066e8894ab64f6ba93c02b0da86aa8
benkhalife <nvioir@gmail.com>
2025-08-13 15:57:46 +0000

    refactor: improve code formatting and add CORS method to Header class

--- src/Header.php
+++ src/Header.php
@@ -1,25 +1,27 @@
 <?php
+
 namespace webrium;
 
 class Header
 {
 
-    public static function all(){
+    public static function all()
+    {
         return getallheaders();
     }
 
 
 
     /** 
-    * Get header Authorization
-    */
+     * Get header Authorization
+     */
     public static function getAuthorizationHeader()
     {
         $headers = null;
         if (isset($_SERVER['Authorization'])) {
-        $headers = trim($_SERVER["Authorization"]);
+            $headers = trim($_SERVER["Authorization"]);
         } else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
-        $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
+            $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
         } elseif (function_exists('apache_request_headers')) {
             $requestHeaders = apache_request_headers();
             // Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
@@ -38,7 +40,7 @@ class Header
     public static function getBearerToken()
     {
         $headers = self::getAuthorizationHeader();
-        
+
         if (!empty($headers)) {
             return substr($headers, 7);
         }
@@ -46,4 +48,22 @@ class Header
         return false;
     }
 
+
+    /**
+     * Set CORS headers
+     * @param string $allowed_origin The origin that is allowed to access the resource. Default is "*".
+     * @return void
+     */
+    public static function cors($allowed_origin = "*")
+    {
+        header("Access-Control-Allow-Origin: $allowed_origin");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization");
+        header("Access-Control-Allow-Credentials: true");
+
+        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+            http_response_code(204); 
+            exit();
+        }
+    }
 }
